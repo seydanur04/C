2f0704634d9fbd78034d5d51670425949c89883d
seydanur04 <143029715+seydanur04@users.noreply.github.com>
2024-11-25 21:34:21 +0300

    Update girilen sayının asal sayı olup olmadığını bulma.cpp

--- girilen sayının asal sayı olup olmadığını bulma.cpp
+++ girilen sayının asal sayı olup olmadığını bulma.cpp
@@ -17,11 +17,11 @@ int main(){
 		return 0;
 	}
 //	-while döngüsü ile bu şekilde						 -for döngüsü ile bu şekilde
-	while(i< sayi/2){									  for(i=2; i<=sayi/2; i++){														}
-		if(sayi%i == 0){								  	  if(sayi%i == 0){
-			printf("asal sayi olamaz.",sayi,i);					  printf("asal sayi olamaz.",sayi,i);
-			return 0;											  return 0;
-		}													  }
+	while(i< sayi/2){							  for(i=2; i<=sayi/2; i++){														}
+		if(sayi%i == 0){						 	 if(sayi%i == 0){
+			printf("asal sayi olamaz.",sayi,i);					printf("asal sayi olamaz.",sayi,i);
+			return 0;								return 0;
+		}								    	  }
 		i++;
 	}
 	printf("bu bir asal sayidir.", sayi);
