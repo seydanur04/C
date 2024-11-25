56aa53e52388f3a93e7e448bfbabc2339cda58b6
seydanur04 <143029715+seydanur04@users.noreply.github.com>
2024-11-25 21:19:21 +0300

    Add files via upload

---
+++ basit çarpım tablosu.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+// döngüler ile çarpým tablosu
+
+int main(){
+	
+	int sayi,i;
+	sayi = 0;
+	i = 1;
+	
+	
+	printf("1 ile 10 arasi sayi gir\n");
+	scanf("%d",&sayi);
+	
+	if(sayi<1 || sayi>10){
+		printf("lütfen geçerli sayi giriniz");
+		return 0;
+	}
+		
+	while (i<=10){
+		printf("%d x %d\n",sayi,i, sayi*i);
+		i++;
+	}
+	
+	
+	
+}
